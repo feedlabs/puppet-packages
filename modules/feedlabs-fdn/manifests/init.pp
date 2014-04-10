025c8a416d53d5d0aa8468c833736c8b2f303fe5
Chris Stasiak <me@krzysztof-stasiak.com>
2014-04-10 15:22:26 +0200

    Some common updated for feedlabs

--- modules/feedlabs-fdn/manifests/init.pp
+++ modules/feedlabs-fdn/manifests/init.pp
@@ -1,6 +1,13 @@
 define feedlabs-fdn (
   $version = '0.0.1',
+  $host = '127.0.0.1',
+  $port = '22001',
   $configuration = []
+  $nsq_config = []
 ) {
+
+  # should be a deamon
+  # chould have a monit
+  # should have a bipbip
 
 }
