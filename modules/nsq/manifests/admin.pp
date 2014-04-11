d3912b76f6af3467d7bce68bf439704ef5f86c96
Chris Stasiak <me@krzysztof-stasiak.com>
2014-04-11 18:46:50 +0200

    Fixed config file for nsqadmin

--- modules/nsq/manifests/admin.pp
+++ modules/nsq/manifests/admin.pp
@@ -2,11 +2,15 @@ class nsq::admin (
   $version = '0.2.27',
   $address = '0.0.0.0',
   $port    = '4171',
-  $nsqd_http_address = [],
-  $lookupd_http_address = []
+  $nsqd_http_addresses = [],
+  $nsqlookupd_http_addresses = []
 ) {
 
   include 'nsq'
+
+  if !count($nsqd_http_addresses) and !count($nsqlookupd_http_addresses) {
+    fails('nsqd_http_addresses or lookupd_http_addresses must be provided!')
+  }
 
   file {'/etc/nsq/nsqadmin.conf':
     ensure => file,
