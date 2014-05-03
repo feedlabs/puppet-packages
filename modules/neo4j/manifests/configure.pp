0f62d402efe73d86710132c25fd3f4b74845a7c0
Chris Stasiak <me@krzysztof-stasiak.com>
2014-05-03 16:27:19 +0200

    Fixed lint problems

--- modules/neo4j/manifests/configure.pp
+++ modules/neo4j/manifests/configure.pp
@@ -4,8 +4,8 @@ class neo4j::configure (
 
   # Make sure Neo4j is accessible from outside the VM
   exec { 'configure network':
-    onlyif	=> "grep '#org.neo4j.server.webserver.address=0.0.0.0' ${install_dir}/conf/neo4j-server.properties",
-    command	=> "sed -i -e s/'#org.neo4j.server.webserver.address=0.0.0.0'/'org.neo4j.server.webserver.address=0.0.0.0'/g ${install_dir}/conf/neo4j-server.properties",
+    onlyif  => "grep '#org.neo4j.server.webserver.address=0.0.0.0' ${install_dir}/conf/neo4j-server.properties",
+    command => "sed -i -e s/'#org.neo4j.server.webserver.address=0.0.0.0'/'org.neo4j.server.webserver.address=0.0.0.0'/g ${install_dir}/conf/neo4j-server.properties",
     path    => '/bin',
     notify  => Service['neo4j-service'],
   }
