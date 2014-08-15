499d470b9efb4dc121b0c6326508564df23ba9b0
Chris Stasiak <me@krzysztof-stasiak.com>
2014-08-15 18:38:08 +0200

    General fixes

--- modules/feedify/files/test/index.go
+++ modules/feedify/files/test/index.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/astaxie/beego"
+import (
+	"flag"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	const (
+		defaultPort = 8080
+		usage       = "set port"
+	)
+	flag.IntVar(&beego.HttpPort, "port", defaultPort, usage)
+}
 
 func main() {
+    flag.Parse()
     beego.Run()
 }
