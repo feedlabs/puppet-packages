c18b12f33e4038acd2ffe605b76978fa0b94b843
Chris Stasiak <me@krzysztof-stasiak.com>
2014-08-31 15:29:34 +0200

    Upgraded golang version for feedify (1.3.1)

--- modules/feedify/manifests/server-dev.pp
+++ modules/feedify/manifests/server-dev.pp
@@ -11,7 +11,7 @@ class feedify::server-dev (
   $daemon_args = "--config /etc/feedify/feedify.conf --port ${port}"
 
   class {'golang':
-    version => '1.2',
+    version => '1.3.1',
     gopath => $go_path,
     require => User['feedify'],
   }
