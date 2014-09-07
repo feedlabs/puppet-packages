b9960928581cd94ff6b2006b287c9a3f419fa21e
Chris Stasiak <me@krzysztof-stasiak.com>
2014-09-07 15:44:41 +0200

    File as virtual resource

--- modules/golang/manifests/dev.pp
+++ modules/golang/manifests/dev.pp
@@ -2,14 +2,13 @@ define golang::dev (
   $go_path
 ) {
 
-  file {"create gopath dir for ${name}":
+  @file {"create gopath dir for ${name}":
     path => $go_path,
     ensure => directory,
     mode => 0644,
     owner => 0,
     group => 0,
   }
-  ->
 
   file {"create golang profile for ${name}":
     path => "/etc/profile.d/golang-${name}.sh",
